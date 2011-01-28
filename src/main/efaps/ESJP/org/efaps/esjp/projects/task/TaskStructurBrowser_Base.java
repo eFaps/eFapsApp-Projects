8350cab2f73c67374f9da9e6beb437a5886d799a
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-01-28 21:53:37 +0000

    - adding StructurBrowser for Tasks in Project
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/projects/trunk@6096 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/task/TaskStructurBrowser_Base.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003 - 2011 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.projects.task;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.esjp.ui.structurbrowser.StandartStructurBrowser;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("d8664446-22f0-4d7f-af76-0e5137889d87")
+@EFapsRevision("$Rev$")
+public abstract class TaskStructurBrowser_Base
+    extends StandartStructurBrowser
+{
+
+}
