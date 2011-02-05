35205b8d2442187d4f001a940d9ae2fc5415a676
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-02-05 03:45:37 +0000

    - adding first chart examples for progress of tasks in projects
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/projects/trunk@6124 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/task/Progress_Base.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2003 - 2011 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.projects.task;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.esjp.common.chart.LineChart;
+import org.efaps.util.EFapsException;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("ec8afa02-acd5-468b-bb72-af1f5685b777")
+@EFapsRevision("$Rev$")
+public abstract class Progress_Base
+{
+    public Return getChartFieldValue(final Parameter _parameter)
+        throws EFapsException
+    {
+        final LineChart linechart = new LineChart();
+        final Return ret = linechart.execute(_parameter);
+        return ret;
+    }
+}
