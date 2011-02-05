c3c8580db59a141b78f7985168ed79b25ee0df54
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-02-05 23:20:04 +0000

    - update of progress chart esjp in projects
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/projects/trunk@6127 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/projects/task/Progress_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/task/Progress_Base.java
@@ -18,16 +18,33 @@
  * Last Changed By: $Author$
  */
 
-
 package org.efaps.esjp.projects.task;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.efaps.admin.datamodel.Dimension;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.PrintQuery;
+import org.efaps.db.QueryBuilder;
+import org.efaps.esjp.ci.CIProjects;
 import org.efaps.esjp.common.chart.LineChart;
 import org.efaps.util.EFapsException;
-
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.CustomXYToolTipGenerator;
+import org.jfree.chart.labels.XYToolTipGenerator;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.RegularTimePeriod;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.joda.time.DateTime;
 
 /**
  * TODO comment!
@@ -39,11 +56,104 @@ import org.efaps.util.EFapsException;
 @EFapsRevision("$Rev$")
 public abstract class Progress_Base
 {
+
     public Return getChartFieldValue(final Parameter _parameter)
         throws EFapsException
     {
-        final LineChart linechart = new LineChart();
+        final ProgressChart linechart = new ProgressChart();
         final Return ret = linechart.execute(_parameter);
         return ret;
     }
+
+    public class ProgressChart
+        extends LineChart
+    {
+
+        @Override
+        protected JFreeChart createChart(final Parameter _parameter,
+                                         final XYDataset _dataset,
+                                         final XYToolTipGenerator _toolTipGen)
+            throws EFapsException
+        {
+
+            final JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                            getTitel(_parameter),
+                            getXAxisLabel(_parameter),
+                            getYAxisLabel(_parameter),
+                            _dataset,
+                            true,
+                            true,
+                            false
+                            );
+            return chart;
+        }
+
+        @Override
+        protected String getYAxisLabel(final Parameter _parameter)
+            throws EFapsException
+        {
+            final PrintQuery print = new PrintQuery(_parameter.getInstance());
+            print.addAttribute(CIProjects.TaskScheduled.UoM);
+            print.execute();
+            final Long uoMId = print.<Long>getAttribute(CIProjects.TaskScheduled.UoM);
+            return Dimension.getUoM(uoMId).getName();
+        }
+
+        @Override
+        protected XYDataset getDataSet(final Parameter _parameter)
+        {
+            return new TimeSeriesCollection();
+        }
+
+        @Override
+        protected void fillData(final Parameter _parameter,
+                                final XYDataset _dataset,
+                                final CustomXYToolTipGenerator _ttg)
+            throws EFapsException
+        {
+            final PrintQuery print = new PrintQuery(_parameter.getInstance());
+            print.addAttribute(CIProjects.TaskScheduled.Quantity, CIProjects.TaskScheduled.DateFrom,
+                            CIProjects.TaskScheduled.DateUntil);
+            print.execute();
+            final BigDecimal quantity = print.<BigDecimal>getAttribute(CIProjects.TaskScheduled.Quantity);
+            final DateTime until = print.<DateTime>getAttribute(CIProjects.TaskScheduled.DateUntil);
+            final DateTime from = print.<DateTime>getAttribute(CIProjects.TaskScheduled.DateFrom);
+
+            final TimeSeries series = new TimeSeries("First");
+            final List<String> toolTips = new ArrayList<String>();
+            final RegularTimePeriod t = new Day(from.toDate());
+            series.add(t, 0);
+            toolTips.add("0");
+            series.add(new Day(until.toDate()), quantity);
+            toolTips.add(quantity.toPlainString());
+//            while (t.getEnd().before(until.toDate())) {
+//                series.add(t, 55);
+//                t = t.next();
+//            }
+            ((TimeSeriesCollection) _dataset).addSeries(series);
+            _ttg.addToolTipSeries(toolTips);
+            final QueryBuilder queryBldr = new QueryBuilder(CIProjects.ProgressTaskAbstract);
+            queryBldr.addWhereAttrEqValue(CIProjects.ProgressTaskAbstract.TaskAbstractLink,
+                            _parameter.getInstance().getId());
+            queryBldr.addOrderByAttributeAsc(CIProjects.ProgressTaskAbstract.Date);
+            final MultiPrintQuery multi = queryBldr.getPrint();
+            multi.setEnforceSorted(true);
+            multi.addAttribute(CIProjects.ProgressTaskAbstract.Date,
+                            CIProjects.ProgressTaskAbstract.UoM,
+                            CIProjects.ProgressTaskAbstract.Progress);
+            if (multi.execute()) {
+                final TimeSeries series2 = new TimeSeries("Second");
+                ((TimeSeriesCollection) _dataset).addSeries(series2);
+                final List<String> toolTips2 = new ArrayList<String>();
+                _ttg.addToolTipSeries(toolTips2);
+                while (multi.next()) {
+                    final DateTime date = multi.<DateTime>getAttribute(CIProjects.ProgressTaskAbstract.Date);
+                    final BigDecimal value = multi.<BigDecimal>getAttribute(CIProjects.ProgressTaskAbstract.Progress);
+                    series2.addOrUpdate(new Day(date.toDate()), value);
+                    toolTips2.add(value.toPlainString());
+                }
+            }
+        }
+    }
+
 }
