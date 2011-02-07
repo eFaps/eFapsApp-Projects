ae410f5e41659432eeb5d6632f6cc87fa07cd0f0
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-02-07 23:10:23 +0000

    - adding analyse of subtask and weigted tasks to projects
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/projects/trunk@6134 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/projects/task/Progress_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/task/Progress_Base.java
@@ -23,6 +23,8 @@ package org.efaps.esjp.projects.task;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.efaps.admin.datamodel.Dimension;
 import org.efaps.admin.dbproperty.DBProperties;
@@ -31,9 +33,12 @@ import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Context;
+import org.efaps.db.Instance;
+import org.efaps.db.InstanceQuery;
 import org.efaps.db.MultiPrintQuery;
 import org.efaps.db.PrintQuery;
 import org.efaps.db.QueryBuilder;
+import org.efaps.db.SelectBuilder;
 import org.efaps.esjp.ci.CIProjects;
 import org.efaps.esjp.common.chart.LineChart;
 import org.efaps.util.EFapsException;
@@ -145,6 +150,7 @@ public abstract class Progress_Base
             toolTips.add(untilDate + " - " + quantity.toPlainString());
             ((TimeSeriesCollection) _dataset).addSeries(series);
             _ttg.addToolTipSeries(toolTips);
+            //get the progress for this task
             final QueryBuilder queryBldr = new QueryBuilder(CIProjects.ProgressTaskAbstract);
             queryBldr.addWhereAttrEqValue(CIProjects.ProgressTaskAbstract.TaskAbstractLink,
                             _parameter.getInstance().getId());
@@ -168,8 +174,159 @@ public abstract class Progress_Base
                                     formatter.withLocale(Context.getThreadContext().getLocale()));
                     toolTips2.add(dateStr + " - " + value.toPlainString());
                 }
+
+            }
+            final ProgressSeries progSeries = getSubTaskProgressSeries(_parameter, _parameter.getInstance(), from,
+                            until);
+            if (!progSeries.isEmpty()) {
+                final TimeSeries series3 = new TimeSeries(DBProperties.getProperty(
+                                "org.efaps.esjp.projects.task.Progress.subSeries"));
+                ((TimeSeriesCollection) _dataset).addSeries(series3);
+                final List<String> toolTips3 = new ArrayList<String>();
+                _ttg.addToolTipSeries(toolTips3);
+                for (final Entry<DateTime, BigDecimal> entry : progSeries.entrySet()) {
+
+                    series3.addOrUpdate(new Day(entry.getKey().toDate()), entry.getValue());
+                    final String dateStr = entry.getKey().withChronology(Context.getThreadContext().getChronology())
+                                    .toString(formatter.withLocale(Context.getThreadContext().getLocale()));
+                    toolTips3.add(dateStr + " - " + entry.getValue().toPlainString());
+                }
             }
         }
     }
 
+    protected ProgressSeries getSubTaskProgressSeries(final Parameter _parameter,
+                                                      final Instance _taskInstance,
+                                                      final DateTime _from,
+                                                      final DateTime _until)
+        throws EFapsException
+    {
+        final List<ProgressSeries> seriesCollection = new ArrayList<ProgressSeries>();
+        final QueryBuilder queryBldr = new QueryBuilder(CIProjects.TaskAbstract);
+        queryBldr.addWhereAttrEqValue(CIProjects.TaskAbstract.ParentTaskAbstractLink, _taskInstance.getId());
+        final InstanceQuery query = queryBldr.getQuery();
+        query.execute();
+        while (query.next()) {
+            ProgressSeries series;
+            final QueryBuilder prQueryBldr = new QueryBuilder(CIProjects.ProgressTaskAbstract);
+            prQueryBldr.addWhereAttrEqValue(CIProjects.ProgressTaskAbstract.TaskAbstractLink,
+                            query.getCurrentValue().getId());
+            final MultiPrintQuery multi = prQueryBldr.getPrint();
+            multi.setEnforceSorted(true);
+            final SelectBuilder selQuan = new SelectBuilder().linkto(CIProjects.ProgressTaskAbstract.TaskAbstractLink)
+                .attribute(CIProjects.TaskAbstract.Quantity);
+            final SelectBuilder selWeight = new SelectBuilder().linkto(CIProjects.ProgressTaskAbstract.TaskAbstractLink)
+                .attribute(CIProjects.TaskAbstract.Weight);
+            multi.addSelect(selQuan, selWeight);
+            multi.addAttribute(CIProjects.ProgressTaskAbstract.Date,
+                            CIProjects.ProgressTaskAbstract.UoM,
+                            CIProjects.ProgressTaskAbstract.Progress);
+            if (multi.execute()) {
+                series = new ProgressSeries();
+                while (multi.next()) {
+                    final DateTime date = multi.<DateTime>getAttribute(CIProjects.ProgressTaskAbstract.Date);
+                    final BigDecimal value = multi.<BigDecimal>getAttribute(CIProjects.ProgressTaskAbstract.Progress);
+                    final BigDecimal quantity = multi.<BigDecimal>getSelect(selQuan);
+                    final BigDecimal weight = multi.<BigDecimal>getSelect(selWeight);
+                    series.setWeight(weight);
+                    final BigDecimal current;
+                    if (quantity != null) {
+                        current = value.multiply(new BigDecimal(100).setScale(8).divide(quantity, BigDecimal.ROUND_HALF_UP));
+                    } else {
+                        current = BigDecimal.ZERO;
+                    }
+                    series.put(date, current);
+                }
+            } else {
+                series = getSubTaskProgressSeries(_parameter, query.getCurrentValue(), _from, _until);
+            }
+            seriesCollection.add(series);
+        }
+        return combineProgressSeries(_parameter, seriesCollection, _from, _until);
+    }
+
+
+    public ProgressSeries combineProgressSeries(final Parameter _parameter,
+                                                final List<ProgressSeries> _seriesCollection,
+                                                final DateTime _from,
+                                                final DateTime _until)
+    {
+        final ProgressSeries ret = new ProgressSeries();
+        DateTime current = _from;
+        if (!_seriesCollection.isEmpty()) {
+            while (current.isBefore(_until)) {
+                boolean hasValues = false;
+                BigDecimal value = BigDecimal.ZERO;
+                BigDecimal weights = BigDecimal.ZERO;
+                for (final ProgressSeries series : _seriesCollection) {
+                    if (!hasValues) {
+                        hasValues = series.containsKey(current);
+                    }
+                    weights = weights.add(series.getWeight());
+                    value = value.add(series.getWeight().multiply(series.get(current)));
+                }
+                if (value.compareTo(BigDecimal.ZERO) != 0 && hasValues) {
+                    ret.put(current, value.divide(weights, BigDecimal.ROUND_HALF_UP));
+                } else if (!hasValues && current.equals(_from)) {
+                    ret.put(current, BigDecimal.ZERO);
+                }
+                current = current.plusDays(1);
+            }
+        }
+        return ret;
+    }
+
+    public class ProgressSeries
+        extends TreeMap<DateTime, BigDecimal>
+    {
+        private BigDecimal weight = BigDecimal.ONE;
+
+        /**
+         * Getter method for the instance variable {@link #weight}.
+         *
+         * @return value of instance variable {@link #weight}
+         */
+        protected BigDecimal getWeight()
+        {
+            return this.weight;
+        }
+
+
+        /**
+         * Setter method for instance variable {@link #weight}.
+         *
+         * @param _weight value for instance variable {@link #weight}
+         */
+
+        protected void setWeight(final BigDecimal _weight)
+        {
+            if (_weight != null) {
+                this.weight = _weight;
+            }
+        }
+
+        /**
+         *
+         */
+        private static final long serialVersionUID = 1L;
+
+        private BigDecimal last = BigDecimal.ZERO;
+
+        /* (non-Javadoc)
+         * @see java.util.TreeMap#get(java.lang.Object)
+         */
+        @Override
+        public BigDecimal get(final Object _key)
+        {
+            BigDecimal ret;
+            if (containsKey(_key)) {
+                ret = super.get(_key);
+                this.last = ret;
+            } else {
+                ret = this.last;
+            }
+            return ret;
+        }
+
+    }
 }
