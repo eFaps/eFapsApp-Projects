3f5d5bf219607cb9d5e33678504e745c174e8807
jorge.cueva@moxter.net <jorge.cueva@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-12-13 17:00:43 +0000

    - add method to choose the lead in a form.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/projects/trunk@7303 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/projects/Project_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/Project_Base.java
@@ -31,6 +31,7 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.efaps.admin.datamodel.Classification;
 import org.efaps.admin.datamodel.Status;
 import org.efaps.admin.datamodel.Type;
+import org.efaps.admin.datamodel.ui.FieldValue;
 import org.efaps.admin.dbproperty.DBProperties;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Parameter.ParameterValues;
@@ -38,6 +39,11 @@ import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.admin.ui.AbstractUserInterfaceObject.TargetMode;
+import org.efaps.admin.user.Group;
+import org.efaps.admin.user.Role;
+import org.efaps.ci.CIAdminUser;
+import org.efaps.db.AttributeQuery;
 import org.efaps.db.Context;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
@@ -561,4 +567,121 @@ public abstract class Project_Base
         return new Return();
     }
 
+    public Return getSalesPersonFieldValue(final Parameter _parameter)
+        throws EFapsException
+    {
+        final org.efaps.esjp.common.uiform.Field field = new org.efaps.esjp.common.uiform.Field() {
+
+            @Override
+            protected DropDownPosition getDropDownPosition(final Parameter _parameter,
+                                                           final Object _value,
+                                                           final Object _option)
+                throws EFapsException
+            {
+                final Map<?, ?> props = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+                final FieldValue fieldValue = (FieldValue) _parameter.get(ParameterValues.UIOBJECT);
+                DropDownPosition pos;
+                if (TargetMode.EDIT.equals(fieldValue.getTargetMode())) {
+                    pos = new DropDownPosition(_value, _option) {
+                        @Override
+                        public boolean isSelected()
+                        {
+                            boolean ret = false;
+                            final Long persId = (Long) fieldValue.getValue();
+                            ret = getValue().equals(persId);
+                            return ret;
+                        }
+                    };
+                } else {
+                    if ("true".equalsIgnoreCase((String) props.get("SelectCurrent"))) {
+                        pos = new DropDownPosition(_value, _option) {
+
+                            @Override
+                            public boolean isSelected()
+                            {
+                                boolean ret = false;
+                                long persId = 0;
+                                try {
+                                    persId = Context.getThreadContext().getPerson().getId();
+                                } catch (final EFapsException e) {
+                                    // nothing must be done at all
+                                    e.printStackTrace();
+                                }
+                                ret = new Long(persId).equals(getValue());
+                                return ret;
+                            }
+                        };
+                    } else {
+                        pos = super.getDropDownPosition(_parameter, _value, _option);
+                    }
+                }
+                return pos;
+            }
+
+            @Override
+            protected void add2QueryBuilder4List(final Parameter _parameter,
+                                                 final QueryBuilder _queryBldr)
+                throws EFapsException
+            {
+                final Map<?, ?> props = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+                final String rolesStr = (String) props.get("Roles");
+                final String groupsStr = (String) props.get("Groups");
+                if (rolesStr != null && !rolesStr.isEmpty()) {
+                    final String[] roles = rolesStr.split(";");
+                    final List<Long> roleIds = new ArrayList<Long>();
+                    for (final String role : roles) {
+                        final Role aRole = Role.get(role);
+                        if (aRole != null) {
+                            roleIds.add(aRole.getId());
+                        }
+                    }
+                    if (!roleIds.isEmpty()) {
+                        final QueryBuilder queryBldr = new QueryBuilder(CIAdminUser.Person2Role);
+                        queryBldr.addWhereAttrEqValue(CIAdminUser.Person2Role.UserToLink, roleIds.toArray());
+
+                        _queryBldr.addWhereAttrInQuery(CIAdminUser.Abstract.ID,
+                                        queryBldr.getAttributeQuery(CIAdminUser.Person2Role.UserFromLink));
+                    }
+                }
+                if (groupsStr != null && !groupsStr.isEmpty()) {
+                    final String[] groups;
+                    boolean and = true;
+                    if (groupsStr.contains("|")) {
+                        groups = groupsStr.split("\\|");
+                    } else {
+                        groups = groupsStr.split(";");
+                        and = false;
+                    }
+
+                    final List<Long> groupIds = new ArrayList<Long>();
+                    for (final String group : groups) {
+                        final Group aGroup = Group.get(group);
+                        if (aGroup != null) {
+                            groupIds.add(aGroup.getId());
+                        }
+                    }
+                    if (!groupIds.isEmpty()) {
+                        if (and) {
+                            for (final Long group : groupIds) {
+                                final QueryBuilder queryBldr = new QueryBuilder(CIAdminUser.Person2Group);
+                                queryBldr.addWhereAttrEqValue(CIAdminUser.Person2Group.UserToLink, group);
+                                final AttributeQuery attribute = queryBldr
+                                                        .getAttributeQuery(CIAdminUser.Person2Group.UserFromLink);
+                                _queryBldr.addWhereAttrInQuery(CIAdminUser.Abstract.ID, attribute);
+                            }
+                        } else {
+                            final QueryBuilder queryBldr = new QueryBuilder(CIAdminUser.Person2Group);
+                            queryBldr.addWhereAttrEqValue(CIAdminUser.Person2Group.UserToLink, groupIds.toArray());
+                            final AttributeQuery attribute = queryBldr
+                                                        .getAttributeQuery(CIAdminUser.Person2Group.UserFromLink);
+                            _queryBldr.addWhereAttrInQuery(CIAdminUser.Abstract.ID, attribute);
+                        }
+
+                    }
+                }
+                super.add2QueryBuilder4List(_parameter, _queryBldr);
+            }
+        };
+        return field.dropDownFieldValue(_parameter);
+    }
 }
