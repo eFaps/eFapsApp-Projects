8a17ba857cd1064680ae062baef9b85f8a79adaa
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-02-13 23:32:59 +0000

    - projects: checkstyle only
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/projects/trunk@12038 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/projects/communicationsMatrix/CommunicationsMatrix_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/communicationsMatrix/CommunicationsMatrix_Base.java
@@ -1,7 +1,28 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev: 6402 $
+ * Last Changed:    $Date: 2011-04-05 10:30:11 -0500 (mar, 05 abr 2011) $
+ * Last Changed By: $Author: jan@moxter.net $
+ */
 package org.efaps.esjp.projects.communicationsMatrix;
 
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
 import org.efaps.esjp.ci.CIFormProjects;
@@ -9,6 +30,16 @@ import org.efaps.esjp.ci.CIProjects;
 import org.efaps.util.EFapsException;
 
 
+/**
+ * This class must be replaced for customization, therefore it is left empty.
+ * Functional description can be found in the related "<code>_base</code>"
+ * class.
+ *
+ * @author The eFaps Team
+ * @version $Id: DocumentAbstract.java 5526 2010-09-10 14:17:54Z miguel.a.aranya $
+ */
+@EFapsUUID("d8c366c8-5a5c-43b8-80e2-708d4511c666")
+@EFapsRevision("$Rev: 5526 $")
 public class CommunicationsMatrix_Base
 {
 
