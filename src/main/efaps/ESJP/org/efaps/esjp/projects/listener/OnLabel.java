1a62eb5a48eb8185b2dfc50183746cc516b3f8c0
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-08-24 00:28:12 +0000

    - projects: adding label evaluation interface
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/projects/trunk@13813 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/listener/OnLabel.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.projects.listener;
+
+import org.efaps.admin.program.esjp.EFapsListener;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+
+/**
+ * This class must be replaced for customization, therefore it is left empty.
+ * Functional description can be found in the related "<code>_base</code>"
+ * class.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("7ab47d5d-6a39-46b5-9205-e52c008e170c")
+@EFapsRevision("$Rev$")
+@EFapsListener
+public class OnLabel
+    extends OnLabel_Base
+{
+
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/listener/OnLabel_Base.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.projects.listener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Instance;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.QueryBuilder;
+import org.efaps.db.SelectBuilder;
+import org.efaps.esjp.accounting.listener.IOnLabel;
+import org.efaps.esjp.ci.CIProjects;
+import org.efaps.util.EFapsException;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("97ff2c4a-0e28-421f-9787-2f3a20188ec5")
+@EFapsRevision("$Rev$")
+public abstract class OnLabel_Base
+    implements IOnLabel
+{
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @param _instance of the document to be evaluated
+     * @return collection of instances
+     * @throws EFapsException on error
+     */
+    @Override
+    public Collection<? extends Instance> evalLabelsForDocument(final Parameter _parameter,
+                                                                final Instance _instance)
+        throws EFapsException
+    {
+        final List<Instance> ret = new ArrayList<>();
+        final QueryBuilder attrQueryBldr = new QueryBuilder(CIProjects.Project2DocumentAbstract);
+        attrQueryBldr.addWhereAttrEqValue(CIProjects.Project2DocumentAbstract.ToAbstract, _instance);
+
+        final QueryBuilder queryBldr = new QueryBuilder(CIProjects.ProjectService2Label);
+        queryBldr.addWhereAttrInQuery(CIProjects.ProjectService2Label.FromLink,
+                        attrQueryBldr.getAttributeQuery(CIProjects.Project2DocumentAbstract.FromAbstract));
+        final MultiPrintQuery multi = queryBldr.getPrint();
+        final SelectBuilder selInst = SelectBuilder.get().linkto(CIProjects.ProjectService2Label.ToLink).instance();
+        multi.addSelect(selInst);
+        multi.execute();
+        while (multi.next()) {
+            ret.add(multi.<Instance>getSelect(selInst));
+        }
+        return ret;
+    }
+
+    @Override
+    public int getWeight()
+    {
+        return 0;
+    }
+}
